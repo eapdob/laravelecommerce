6b9b8b448230e42db8efbb3cea572b769d308659
Evgenii Poperezhai <eapdob@gmail.com>
2022-08-29 12:14:10 +0300

    - category -- add setCategoryActive

--- app/helpers.php
+++ app/helpers.php
@@ -7,3 +7,7 @@ function presentPrice($price)
     $fmt = new \NumberFormatter('en_US', \NumberFormatter::CURRENCY);
     return str_replace(',', '', $fmt->formatCurrency($price / 100, 'USD'));
 }
+
+function setCategoryActive($slug) {
+    return request()->category === $slug ? 'active' : '';
+}
