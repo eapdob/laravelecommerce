67b198262c31c5d916ebd1fd0d457183bca40d81
Evgenii Poperezhai <eapdob@gmail.com>
2023-04-30 16:49:51 +0300

    - added categories to admin product page (checkboxes)

--- app/Models/CategoryProduct.php
+++ app/Models/CategoryProduct.php
@@ -10,4 +10,9 @@ class CategoryProduct extends Model
     use HasFactory;
 
     protected $table = 'category_product';
+
+    protected $fillable = [
+        'product_id',
+        'category_id'
+    ];
 }
